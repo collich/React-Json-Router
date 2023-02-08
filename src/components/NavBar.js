ffa3db4710be845ce593826efab3d0d8947ae308
Collin <collin-chua@hotmail.com>
2023-02-08 21:36:32 +0800

    First tailwindcss implementation..tldr very bad first time

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,17 @@
 import { NavLink } from "react-router-dom"
+import classNames from "classnames"
 
 const NavBar = () => {
+    const classNameElement = (isActive) => {
+        const classNameE = classNames({"text-amber-500 bg-gray-400 rounded-lg": isActive, "text-gray-500": !isActive}, "text-xl p-2.5")
+        return classNameE
+    }
+
     return (
-        <div>
-            <nav>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
+        <div className="flex justify-center">
+            <nav className="border p-2 mx-auto my-5 inline-block rounded-lg">
+                <NavLink to="/" className={({isActive}) => classNameElement(isActive) }>Home</NavLink>
+                <NavLink to="/about" className={({isActive}) => classNameElement(isActive) }>About</NavLink>
             </nav>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,25 @@
 const Home = () => {
     return (
-        <div>Home Page</div>
+        <div>
+            <h1 className="text-red-500 font-bold text-center text-2xl">Home Page</h1>
+            <hr className="h-px my-8 mx-auto bg-gray-200 border-0 dark:bg-gray-700 w-3/4"></hr>
+            <div className="flex justify-center border border-black m-10 rounded-2xl justify-around">
+                
+                <div className="border w-1/3 p-5 m-5 border-gray-500 rounded-2xl w-full h-full">
+                    <p>Something about the home page</p>
+                </div>
+
+                <div className="w-1/4 w-full">
+                    <div className="border w-100 p-5 m-5 border-gray-500 rounded-2xl">
+                        <p>Other Details</p>
+                    </div>
+
+                    <div className="border w-100 p-5 m-5 border-gray-500 rounded-2xl">
+                        <p>Some details</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
